3a202a36ff7375b9d8787a8f3959a62ddcbaccfb
Darío González <dario-goirad@hotmail.com>
2018-04-18 22:16:29 -0300

    Solucion Variables_operaciones

---
+++ Variables_operaciones/Variables_operaciones/InteresesProducidos.vb
@@ -0,0 +1,19 @@
+﻿Module InteresesProducidos1
+    Dim monto As Integer
+    Dim interes As Single
+    Dim dias As Integer
+    Sub Main()
+        Console.WriteLine("Ingrese el monto: ")
+        monto = Console.ReadLine()
+
+        Console.WriteLine("Ingrese el interes: ")
+        interes = Console.ReadLine()
+
+        Console.WriteLine("Ingrese los dias: ")
+        dias = Console.ReadLine()
+
+        Dim InteresesProducidos As Single = (monto * interes * dias) / (360 * 100)
+
+        Console.WriteLine("Intereses Producidos | (monto * interes * dias) / (360 * 100) = " & InteresesProducidos)
+    End Sub
+End Module
