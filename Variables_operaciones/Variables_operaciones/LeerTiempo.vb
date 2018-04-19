caf6107c4f3ad1124ec640c783adaf3b8416dfa4
Darío González <dario-goirad@hotmail.com>
2018-04-18 22:07:31 -0300

    Solucion Variables_opraciones (Aun sin terminar)

---
+++ Variables_operaciones/Variables_operaciones/LeerTiempo.vb
@@ -0,0 +1,20 @@
+﻿Module LeerTiempo
+    Dim hora As Integer
+    Dim dia As Integer
+    Sub Main()
+        Dim Hoy As Date
+        Hoy = Now
+
+        Console.WriteLine("Ingrese un día/1 al 31: ")
+        dia = Console.ReadLine()
+
+        Console.WriteLine("Ingrese una Hora/0 a 23hs: ")
+        hora = Console.ReadLine()
+
+        Dim nuevo As New DateTime(Hoy.Year, Hoy.Month, dia, hora, Hoy.Minute, Hoy.Second)
+
+        Console.WriteLine("Resultado: " & nuevo)
+
+    End Sub
+
+End Module
